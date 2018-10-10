0cf628965c77ddeaa12f73f355c2805e053acff9
Senecacherry <42197371+Senecacherry@users.noreply.github.com>
2018-10-09 21:19:48 -0600

    Bare Bones

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+$(document).ready( function() { 
+
+var counter = 0;
+var targetnumber = 88;
+
+$(".RMcry").on("click", function() {
+    
+    counter += 1;
+   
+    alert("your new score is " + counter);
+})}
+);
