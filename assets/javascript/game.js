b28663964f3de783e8defca98fa9dc1107a51c89
Senecacherry <42197371+Senecacherry@users.noreply.github.com>
2018-10-11 19:49:53 -0600

    JS Changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,42 @@
 $(document).ready( function() { 
 
-var counter = 0;
-var targetnumber = Math.floor(Math.random() * 120) + 19;
-var RMcry = Math.floor(Math.random() * 12) +1;
-var RMface = Math.floor(Math.random() * 12) +1;
-var RMfingers = Math.floor(Math.random() * 12) +1;
-var meeseeks = Math.floor(Math.random() * 12) +1;
+    var counter = 0;
+    var targetnumber = Math.floor(Math.random() * 120) + 19;
+    var RMcry = Math.floor(Math.random() * 12) +1;
+    var RMface = Math.floor(Math.random() * 12) +1;
+    var RMfingers = Math.floor(Math.random() * 12) +1;
+    var meeseeks = Math.floor(Math.random() * 12) +1;
+    
+    $("#number-to-guess").text(targetnumber);
 
-$(".RMcry").on("click", function() {
+    $("#the-counter").text(counter);
+
+    $(".RMcry").on("click", function() {
+        
+        counter += RMcry;
+        
+        // alert(RMcry);
     
+    });
+
+    $(".RMface").on("click", function() {
+        
+        counter += RMface;
     
+    });
+
+    $(".RMfingers").on("click", function() {
+        
+        counter += RMfingers;
     
-})}
-);
+    });
+
+    $(".meeseeks").on("click", function() {
+        
+        counter += meeseeks;
+    
+    });
+
+
+
+});
