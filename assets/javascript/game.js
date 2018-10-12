1c21ce3900fb5cc87771bd2a49b124121c7934dc
Senecacherry <42197371+Senecacherry@users.noreply.github.com>
2018-10-11 18:53:16 -0600

    JS Changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,15 @@
 $(document).ready( function() { 
 
 var counter = 0;
-var targetnumber = 88;
+var targetnumber = Math.floor(Math.random() * 120) + 19;
+var RMcry = Math.floor(Math.random() * 12) +1;
+var RMface = Math.floor(Math.random() * 12) +1;
+var RMfingers = Math.floor(Math.random() * 12) +1;
+var meeseeks = Math.floor(Math.random() * 12) +1;
 
 $(".RMcry").on("click", function() {
     
-    counter += 1;
-   
-    alert("your new score is " + counter);
+    
+    
 })}
 );
