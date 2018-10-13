d9382b7b88c15f3356dc7db08f8fc5d5b78198f8
Senecacherry <42197371+Senecacherry@users.noreply.github.com>
2018-10-12 19:38:48 -0600

    Game Running Besides Reset and Loss or Wins

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,111 @@
 $(document).ready( function() { 
 
     var counter = 0;
-    var targetnumber = Math.floor(Math.random() * 120) + 19;
+    var targetNumber = Math.floor(Math.random() * 120) + 19;
     var RMcry = Math.floor(Math.random() * 12) +1;
     var RMface = Math.floor(Math.random() * 12) +1;
     var RMfingers = Math.floor(Math.random() * 12) +1;
     var meeseeks = Math.floor(Math.random() * 12) +1;
     
-    $("#number-to-guess").text(targetnumber);
+    $("#number-to-guess").text(targetNumber);
 
-    $("#the-counter").text(counter);
+    
 
     $(".RMcry").on("click", function() {
         
         counter += RMcry;
         
-        // alert(RMcry);
+        $("#the-counter").html(counter); 
+
+        if (counter === targetNumber) {
+
+    
+            alert("You got schwifty!");
+
+            $("#wins").text(=++);
+
+           
+        }
+
+        else if (counter>= targetNumber) {
+
+    
+
+            alert("Shit on the floor!");
+
+            $("#losses").text(+1);
+            
+    
+        };       
     
     });
 
     $(".RMface").on("click", function() {
         
         counter += RMface;
+
+        $("#the-counter").html(counter);
+
+        if (counter === targetNumber) {
+    
+            alert("You got schwifty!");
+        }
+
+        else if (counter>= targetNumber) {
+
+            alert("Shit on the floor!");
+    
+        };
+
     
     });
 
     $(".RMfingers").on("click", function() {
         
         counter += RMfingers;
+
+        $("#the-counter").html(counter);
+
+        if (counter === targetNumber) {
+
+    
+            alert("You got schwifty!");
+        }
+
+        else if (counter>= targetNumber) {
+
+            alert("Shit on the floor!");
+    
+        }
+
     
     });
 
     $(".meeseeks").on("click", function() {
         
         counter += meeseeks;
+
+        $("#the-counter").html(counter);
+
+        if (counter === targetNumber) {
+
+    
+            alert("You got schwifty!");
+        }
+
+        else if (counter>= targetNumber) {
+
+            alert("Shit on the floor!");
+    
+        }
+
+
+
     
     });
+
+    
+  
 
 
 
